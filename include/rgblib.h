9b0dd92e54661af8f427dd85a8e043a8277038b2
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-30 19:02:35 +0200

    Strlen

--- include/rgblib.h
+++ include/rgblib.h
@@ -6,12 +6,15 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:54:04 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/30 18:57:24 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/30 19:01:05 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RGBLIB_H
 #define RGBLIB_H
+
+// Tools
+size_t	ft_strlen(const char *s);
 
 int	storgb(char *color_str);
 
