7c9584d585763c6b50d91e78e4eff88a6476e402
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-30 19:16:27 +0200

    src/is_rgb_str.c added

--- src/is_rgb_str.c
+++ src/is_rgb_str.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rgblib.h                                           :+:      :+:    :+:   */
+/*   is_rgb_str.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/30 18:54:04 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/30 19:16:14 by jre-gonz         ###   ########.fr       */
+/*   Created: 2022/06/30 19:11:27 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/06/30 19:15:49 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RGBLIB_H
-#define RGBLIB_H
+#include "rgblib.h"
 
-// Tools
-size_t	ft_strlen(const char *s);
+int	is_rgb_str(char *str)
+{
+	size_t	len;
 
-int	is_rgb_str(char *str);
-int	storgb(char *color_str);
-
-#endif
+	if (!str)
+		return (0);
+	len = ft_strlen(str);
+	if (len == 0 || len > 8 || len % 2 != 0)
+		return (0);
+	while (*str)
+	{
+		if ((*str < '0' && *str > '9') || (*str > 'a' && *str < 'b'))
+			return (0);
+		str++;
+	}
+	return (1);
+}
