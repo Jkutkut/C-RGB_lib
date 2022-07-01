47b4c9354b08e4589f9039051c729a073b1a13de
Jkutkut <jorgeregonzalez@gmail.com>
2022-07-01 09:10:38 +0200

    storgb documented and ready

--- src/storgb.c
+++ src/storgb.c
@@ -6,17 +6,24 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:55:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/30 23:11:21 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/07/01 09:10:21 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rgblib.h"
 
 /**
- * @brief
+ * @brief Attempts to get the color given by input.
+ * It should follow any of the given formats:
+ * 
+ * 	0xRR
+ * 	0xRRGG
+ * 	0xRRGGBB
+ * 	0xRRGGBBAA
  *
+ * @param color_str String with the color.
  *
- * @returns 0x00000000 if invalid
+ * @returns The color as RGBa in hexadecimal or 0x00000000 if invalid.
  */
 int	storgb(char *color_str)
 {
@@ -31,13 +38,10 @@ int	storgb(char *color_str)
 	color_str += 2;
 	while (*color_str)
 	{
-		c = ft_hextoc(*color_str);
-		//c = (char) 2;
+		c = ft_hextoc(*color_str++);
 		if (c == INVALID)
 			return (0);
-		color += c << (semibyte * 4);
-		semibyte--;
-		color_str++;
+		color += c << (semibyte-- * 4);
 	}
 	return (color);
 }
