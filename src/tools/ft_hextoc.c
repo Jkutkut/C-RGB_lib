151ff60d23a09bb712793d054fb8983502ce92cb
Jkutkut <jorgeregonzalez@gmail.com>
2022-07-01 09:17:00 +0200

    ft_hextoc documented

--- src/tools/ft_hextoc.c
+++ src/tools/ft_hextoc.c
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_hextoc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 19:18:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/30 23:11:49 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/07/01 09:16:32 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rgblib.h"
 
+/**
+ * @brief Obtains the value in decimal of the char in hexadecimal given.
+ * 
+ * @param c character with the value in hexadecimal.
+ * 
+ * @returns The correct value or INVALID if input is not hexadecimal.
+ */
 char	ft_hextoc(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -20,5 +27,5 @@ char	ft_hextoc(char c)
 		return (c - 'a') + 10;
 	if (c >= 'A' && c <= 'F')
 		return (c - 'A') + 10;
-	return (-1);
+	return (INVALID);
 }
