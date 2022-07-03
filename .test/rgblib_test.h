1d22884b098c8fde422318056103084d701aab57
Jkutkut <jorgeregonzalez@gmail.com>
2022-07-03 11:26:51 +0200

    header file for tests created

---
+++ .test/rgblib_test.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rgblib_test.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/03 11:25:53 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/07/03 11:26:24 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RGBLIB_TEST_H
+#define RGBLIB_TEST_H
+
+#include <stdio.h>
+#include "rgblib.h"
+
+#define NC "\033[0m"
+#define RED "\033[1;31m"
+#define YELLOW "\033[1;33m"
+#define GREEN "\033[1;32m"
+
+#define OK "[OK]"
+#define KO "[KO]"
+
+#endif
--- .test/storgb_test.c
+++ .test/storgb_test.c
@@ -6,22 +6,12 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 19:48:39 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/07/01 09:22:37 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/07/03 11:25:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "rgblib.h"
+#include "rgblib_test.h"
 
-#define NC "\033[0m"
-#define RED "\033[1;31m"
-#define YELLOW "\033[1;33m"
-#define GREEN "\033[1;32m"
-
-#define OK "[OK]"
-#define KO "[KO]"
-
-#define TEST 
 
 void	storgb_test(char *color, int spected)
 {
